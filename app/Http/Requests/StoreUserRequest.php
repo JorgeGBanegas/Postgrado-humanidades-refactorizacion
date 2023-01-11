62ea36c4877a82b06bb75f53ca74180cdae24704
Jorge Gustavo Banegas <jorge.g.banegas@gmail.com>
2023-01-10 23:15:08 -0400

    se tradujo los mensajes de error del formrequest

--- app/Http/Requests/StoreUserRequest.php
+++ app/Http/Requests/StoreUserRequest.php
@@ -31,20 +31,4 @@ class StoreUserRequest extends FormRequest
             'type' => 'required | string | max:255',
         ];
     }
-
-    public function messages()
-    {
-        return [
-            'name.required ' => 'El nombre es obligatorio',
-            'name.string ' => 'El nombre solo debe contener letras',
-            'last_name.required' => 'El apellido es obligatorio',
-            'last_name.string' => 'El apellido solo debe contener letras',
-            'email.required' => 'El email es obligatorio',
-            'email.email' => 'No es un email valido',
-            'password.required' => 'La contraseña es obligatoria',
-            'password.string' => 'La contraseña es obligatoria',
-            'password.confirmed' => 'Las contraseñas no coinciden',
-            'type.required' => 'El tipo de usuario es obligatorio',
-        ];
-    }
 }
--- app/Http/Requests/UpdateUserRequest.php
+++ app/Http/Requests/UpdateUserRequest.php
@@ -30,18 +30,4 @@ class UpdateUserRequest extends FormRequest
             'type' => 'required | string | max:255',
         ];
     }
-
-    public function messages()
-    {
-        return [
-            'name.required ' => 'El nombre es obligatorio',
-            'name.string ' => 'El nombre solo debe contener letras',
-            'last_name.required' => 'El apellido es obligatorio',
-            'last_name.string' => 'El apellido solo debe contener letras',
-            'password.required' => 'La contraseña es obligatoria',
-            'password.string' => 'La contraseña es obligatoria',
-            'password.confirmed' => 'Las contraseñas no coinciden',
-            'type.required' => 'El tipo de usuario es obligatorio',
-        ];
-    }
 }

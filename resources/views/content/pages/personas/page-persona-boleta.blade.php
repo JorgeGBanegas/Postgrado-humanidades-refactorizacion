016fd318241503d63a726295f636f89d14430fe0
Jorge Gustavo Banegas <jorge.g.banegas@gmail.com>
2023-02-13 19:09:20 -0400

    Se cambio el tipo de letra para la boleta deinscripcion

--- resources/views/content/pages/personas/page-persona-boleta.blade.php
+++ resources/views/content/pages/personas/page-persona-boleta.blade.php
@@ -12,43 +12,29 @@
             <h5 class="card-header" style="text-align: center;">Boleta de inscripcion</h5>
         </div>
         @if($tipo == 1)
-        <label> <b> Fecha de Inscripcion: </b>{{$inscripcion->inscrip_program_fecha }}</label>
-        <br>
-        <label> <b> Estudiante: </b> {{$inscripcion->persona->per_nom }} {{$inscripcion->persona->per_appm}}</label>
-        <br>
-        <label> <b> C.I :</b> {{$inscripcion->persona->per_ci }}</label>
-        <br>
-        <label><b> Nombre del programa: </b>{{$inscripcion->program->program_nom }}</label>
-        <br>
-        <label><b> Grupo:</b> {{$inscripcion->grupo_programa->grup_program_cod }}</label>
-        <br>
-        <label><b> Version:</b> {{$inscripcion->grupo_programa->grup_program_vers }}</label>
-        <br>
-        <label><b> Edicion:</b> {{$inscripcion->grupo_programa->grup_program_edic }}</label>
-        <br>
-        <label><b> Horario:</b></label>
-        <br>
+        <h6> <b> Fecha de Inscripcion: </b>{{$inscripcion->inscrip_program_fecha }}</h6>
+        <h6> <b> Estudiante: </b> {{$inscripcion->persona->per_nom }} {{$inscripcion->persona->per_appm}}</h6>
+        <h6> <b> C.I :</b> {{$inscripcion->persona->per_ci }}</h6>
+        <h6><b> Nombre del programa: </b>{{$inscripcion->program->program_nom }}</h6>
+        <h6><b> Grupo:</b> {{$inscripcion->grupo_programa->grup_program_cod }}</h6>
+        <h6><b> Version:</b> {{$inscripcion->grupo_programa->grup_program_vers }}</h6>
+        <h6><b> Edicion:</b> {{$inscripcion->grupo_programa->grup_program_edic }}</h6>
+        <h6><b> Horario:</b></h6>
         @foreach($inscripcion->grupo_programa->horario_programas as $horario)
-        <label style="margin-left: 20px;">{{$horario->hora_program_dia }} [{{$horario->hora_program_hini }} - {{$horario->hora_program_hfin }}] </label>
+        <h6 style="margin-left: 20px;">{{$horario->hora_program_dia }} [{{$horario->hora_program_hini }} - {{$horario->hora_program_hfin }}] </h6>
         <br>
         @endforeach
 
         @else
 
-        <label> <b> Fecha de Inscripcion: </b>{{$inscripcion->inscrip_curs_fecha }}</label>
-        <br>
-        <label> <b> Estudiante: </b> {{$inscripcion->persona->per_nom }} {{$inscripcion->persona->per_appm}}</label>
-        <br>
-        <label> <b> C.I :</b> {{$inscripcion->persona->per_ci }}</label>
-        <br>
-        <label><b> Nombre del curso: </b>{{$inscripcion->curs->curs_nom }}</label>
-        <br>
-        <label><b> Grupo:</b> {{$inscripcion->grupo_curso->grup_curs_cod }}</label>
-        <br>
-        <label><b> Horario:</b></label>
-        <br>
+        <h6> <b> Fecha de Inscripcion: </b>{{$inscripcion->inscrip_curs_fecha }}</h6>
+        <h6> <b> Estudiante: </b> {{$inscripcion->persona->per_nom }} {{$inscripcion->persona->per_appm}}</h6>
+        <h6> <b> C.I :</b> {{$inscripcion->persona->per_ci }}</h6>
+        <h6><b> Nombre del curso: </b>{{$inscripcion->curs->curs_nom }}</h6>
+        <h6><b> Grupo:</b> {{$inscripcion->grupo_curso->grup_curs_cod }}</h6>
+        <h6><b> Horario:</b></h6>
         @foreach($inscripcion->grupo_curso->horario_cursos as $horario)
-        <label style="margin-left: 20px;">{{$horario->hora_curs_dia }} [{{$horario->hora_curs_hini }} - {{$horario->hora_curs_hfin }}] </label>
+        <h6 style="margin-left: 20px;">{{$horario->hora_curs_dia }} [{{$horario->hora_curs_hini }} - {{$horario->hora_curs_hfin }}] </h6>
         <br>
         @endforeach
 
